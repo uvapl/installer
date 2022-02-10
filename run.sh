de82ee4ceab2806f529de9677effec056e40311e
Martijn Stegeman <martijn@stgm.nl>
2022-02-10 11:36:12 +0100

    astyle package for style50

--- run.sh
+++ run.sh
@@ -196,7 +196,7 @@ then
     cross "clang is not installed"
     ohai "Installing make and clang..."
     wait_for_user
-    sudo apt-get install make clang -y
+    sudo apt-get install make clang astyle -y
   fi
 
   which pip3 > /dev/null
