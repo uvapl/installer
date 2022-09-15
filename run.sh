43b924de2cb4d601d2304dbe817af2f33885f5ce
Martijn Stegeman <martijn@stgm.nl>
2022-09-15 10:03:35 +0200

    star

--- run.sh
+++ run.sh
@@ -610,7 +610,7 @@ then
 function check50 ()
 {
   check50_cmd=\$(which check50)
-  output=\$(\$check50_cmd -l \$1 | sed '\$s/file:\/\//\\\\\\\\wsl\\$\\\\Ubuntu/;\$s/\//\\\\/g')
+  output=\$(\$check50_cmd -l \$* | sed '\$s/file:\/\//\\\\\\\\wsl\\$\\\\Ubuntu/;\$s/\//\\\\/g')
   echo "\${output}"
 }
 EOF
