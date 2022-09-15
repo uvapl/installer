0be94be7ae6c8fd380f1935e92c2a8013268404b
Martijn Stegeman <martijn@stgm.nl>
2022-09-15 11:08:45 +0200

    star

--- run.sh
+++ run.sh
@@ -602,12 +602,12 @@ then
   wait_for_user
   sed -i.check50_hack '/^function check50/,/^}$/d' $shell_rc
   cat >> $shell_rc <<-"EOF"
-    function check50 ()
-    {
-      check50_cmd=$(which check50)
-      output=$($check50_cmd -l $* | sed '$s/file:\/\//\\\\wsl\\$\\Ubuntu/;\$s/\//\\/g')
-      echo "${output}"
-    }
+function check50 ()
+{
+  check50_cmd=$(which check50)
+  output=$($check50_cmd -l $* | sed '$s/file:\/\//\\\\wsl\\$\\Ubuntu/;\$s/\//\\/g')
+  echo "${output}"
+}
 EOF
 fi
 
